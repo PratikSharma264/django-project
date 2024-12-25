8bd90d068f1c0079113a1729810633dc900a5a05
Pratik Sharma <sharmapratikabcd@gmail.com>
2024-12-25 18:38:11 +0545

    Intial commit for my Django Project

---
+++ ecomapp/forms.py
@@ -0,0 +1,126 @@
+from django import forms
+from .models import Order,Customer
+from django.forms import TextInput, EmailInput
+from django.contrib.auth.models import User
+import re
+
+class CheckoutForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['ordered_by', 'shipping_address', 'mobile', 'email']
+        widgets = {
+            'mobile': TextInput(attrs={'placeholder': 'Enter your mobile number'}),
+            'email': EmailInput(attrs={'placeholder': 'Enter your email address'}),
+        }
+        help_texts = {
+            'mobile': 'Please enter a 10-digit mobile number.',
+            'email': 'We will send a confirmation to this email address.',
+        }
+
+    def clean_mobile(self):
+        mobile = self.cleaned_data.get('mobile')
+        if not mobile.isdigit() or len(mobile) != 10:  # Phone No. length validation
+            raise forms.ValidationError("Please enter a valid 10-digit mobile number.")
+        return mobile
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email or '@' not in email:  # Basic email validation
+            raise forms.ValidationError("Please enter a valid email address.")
+        return email
+    
+# class CustomerRegistrationForm(forms.ModelForm):
+#     username = forms.CharField(widget=forms.TextInput())
+#     password = forms.CharField(widget=forms.PasswordInput())
+#     email = forms.CharField(widget=forms.EmailInput())
+
+#     class Meta:
+#         model = Customer
+#         fields=["username","password","email","full_name","address"]
+    
+#     def clean_username(self):
+#         uname = self.cleaned_data.get("username")
+#         if User.objects.filter(username=uname).exists():
+#             raise forms.ValidationError("Customer with this name already exists.")
+        
+#         return uname
+
+class CustomerRegistrationForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Enter username", 
+            "class": "form-control"
+        }),
+        label="Username"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Enter password", 
+            "class": "form-control"
+        }),
+        label="Password"
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "placeholder": "Enter email address", 
+            "class": "form-control"
+        }),
+        label="Email Address"
+    )
+
+    class Meta:
+        model = Customer
+        fields = ["username", "password", "email", "full_name", "address"]
+        widgets = {
+            "full_name": forms.TextInput(attrs={
+                "placeholder": "Enter full name", 
+                "class": "form-control"
+            }),
+            "address": forms.TextInput(attrs={
+                "placeholder": "Enter address", 
+                "class": "form-control"
+            })
+        }
+
+    def clean_username(self):
+        uname = self.cleaned_data.get("username")
+        if User.objects.filter(username=uname).exists():
+            raise forms.ValidationError("A user with this username already exists.")
+        return uname
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("An account with this email already exists.")
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        if len(password) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters long.")
+        if not re.search(r"[A-Z]", password):
+            raise forms.ValidationError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[a-z]", password):
+            raise forms.ValidationError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[0-9]", password):
+            raise forms.ValidationError("Password must contain at least one number.")
+        if not re.search(r"[_!@#$%^&*(),.?\":{}|<>]", password):
+            raise forms.ValidationError("Password must contain at least one special character.")
+        return password
+    
+
+class CustomerLoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Enter username", 
+            "class": "form-control"
+        }),
+        label="Username"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Enter password", 
+            "class": "form-control"
+        }),
+        label="Password"
+    )
---
+++ ecomapp/views.py
@@ -0,0 +1,307 @@
+#from typing import Any
+from django.shortcuts import render,redirect
+from django.views.generic import View,TemplateView,CreateView,FormView
+from django.urls import reverse_lazy
+from .models import *
+from django.contrib import messages
+from .forms import CheckoutForm,CustomerRegistrationForm,CustomerLoginForm
+from django.contrib.auth import authenticate, login, logout
+import logging
+
+# Create your views here.
+class EcomMixin(object):
+    def dispatch(self,request,*args,**kwargs):
+        cart_id=request.session.get("cart_id")
+        if cart_id:
+            cart_obj=Cart.objects.get(id=cart_id)
+            if request.user.is_authenticated and request.user.customer:
+                cart_obj.customer=request.user.customer
+                cart_obj.save()
+        return super().dispatch(request,*args,**kwargs)
+    
+
+class HomeView(EcomMixin,TemplateView):
+    template_name= "home.html"
+
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['product_list']=Product.objects.all()
+        context['allcategories']=Category.objects.all().order_by("-id")
+        return context
+    
+class AllProductsView(EcomMixin,TemplateView):
+    template_name="allproducts.html"
+
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['allcategories']=Category.objects.all()
+        return context
+    
+class ProductDetailView(EcomMixin,TemplateView):
+    template_name="productdetail.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        url_slug=self.kwargs['slug']
+        product=Product.objects.get(slug=url_slug)
+        product.view_count+=1
+        product.save()
+        context['product']=product
+        return context
+    
+
+class AddToCartView(EcomMixin,TemplateView):
+    template_name="addtocart.html"
+
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs) 
+        #get product id from requested url
+        product_id=self.kwargs['pro_id']
+    
+        #get product
+        product_obj=Product.objects.get(id=product_id)
+
+        #check if cart exists
+        cart_id=self.request.session.get("cart_id",None)
+        if cart_id:
+            cart_obj=Cart.objects.get(id=cart_id)
+            this_product_in_cart=cart_obj.cartproduct_set.filter(product=product_obj)
+
+            #item already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct=this_product_in_cart.last()
+                cartproduct.quantity +=1
+                cartproduct.subtotal += product_obj.selling_price
+                cartproduct.save()
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+            #new item added in cart
+            else:
+                cartproduct=CartProduct.objects.create(cart=cart_obj, product=product_obj, rate= product_obj.selling_price, quantity=1,
+                subtotal=product_obj.selling_price)
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+        else:
+            cart_obj=Cart.objects.create(total=0)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct=CartProduct.objects.create(cart=cart_obj, product=product_obj, rate= product_obj.selling_price, quantity=1,
+                subtotal=product_obj.selling_price)
+            cart_obj.total += product_obj.selling_price
+            cart_obj.save()
+
+        #check if product already ecists in cart.
+        return context
+        
+
+
+class MyCartView(EcomMixin,TemplateView):
+    template_name='mycart.html'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        cart_id=self.request.session.get("cart_id",None)
+        if cart_id:
+            cart=Cart.objects.get(id=cart_id)
+        else:
+            cart=None
+        context['cart']=cart
+        return context
+        
+
+
+class ManageCartView(EcomMixin,View):
+    def get(self,request,*args, **kwargs):
+        cp_id=self.kwargs['cp_id']
+        action = request.GET.get('action')
+        cp_obj=CartProduct.objects.get(id=cp_id)
+        cart_obj = cp_obj.cart
+       
+        if action=='inc':
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action=='dcr':
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity ==0:
+                    cp_obj.delete()
+        elif action=='rmv':
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass
+        return redirect('ecomapp:mycart')
+
+
+
+class EmptyCartView(EcomMixin,View):
+    def get(self, request, *args, **kwargs):
+        cart_id = request.session.get('cart_id', None)
+        if cart_id:
+            try:
+                cart = Cart.objects.get(id=cart_id)
+                cart.cartproduct_set.all().delete()
+                cart.total = 0
+                cart.save()
+                messages.success(request, "Your cart has been successfully emptied.")
+            except Cart.DoesNotExist:
+                messages.error(request, "The cart could not be found.")
+        else:
+            messages.warning(request, "You don't have an active cart.")
+        
+        return redirect('ecomapp:mycart') 
+
+
+
+
+
+class CheckoutView(EcomMixin,CreateView):
+    template_name = 'checkout.html'
+    form_class = CheckoutForm
+    success_url = reverse_lazy('ecomapp:home')  
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        if request.user.is_authenticated and request.user.customer:
+            pass
+        else:
+            return redirect("/login/?next=/checkout/")
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            try:
+                cart_obj = Cart.objects.get(id=cart_id)
+            except Cart.DoesNotExist:
+                cart_obj = None
+                messages.warning(self.request, "Cart does not exist.")
+        else:
+            cart_obj = None
+        context['cart'] = cart_obj
+        return context
+    
+    def form_valid(self, form):
+        cart_id = self.request.session.get('cart_id')
+        if cart_id:
+            cart_obj= Cart.objects.get(id = cart_id)
+            form.instance.cart = cart_obj
+            form.instance.subtotal=cart_obj.total
+            form.instance.discount =  0
+            form.instance.total = cart_obj.total
+            form.instance.order_status = 'Order Recieved'
+            del self.request.session['cart_id']
+        else:
+            return redirect('ecomapp:home')
+        return super().form_valid(form)
+
+# class CustomerRegistrationView(CreateView):
+#     template_name = "customerregistration.html"
+#     form_class = CustomerRegistrationForm
+#     success_url = reverse_lazy("ecomapp:home")
+    
+#     def form_valid(self, form):
+#         username = form.cleaned_data.get('username')
+#         password = form.cleaned_data.get('password')
+#         email = form.cleaned_data.get('email')
+#         user = User.objects.create_user(username,email,password)
+#         form.instance.user = user
+#         return super().form_valid(form)
+     
+    
+# 
+
+logger = logging.getLogger(__name__)
+
+class CustomerRegistrationView(CreateView):
+    template_name = "customerregistration.html"
+    form_class = CustomerRegistrationForm
+    success_url = reverse_lazy("ecomapp:home")
+
+    def form_valid(self, form):
+        try:
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            email = form.cleaned_data.get('email')
+            user = User.objects.create_user(username, email, password)
+            form.instance.user = user
+            login(self.request, user)
+            messages.success(self.request, "Your account has been created and you are now logged in!")
+
+        except Exception as e:
+            logger.error(f"Error during registration: {e}")
+
+            messages.error(self.request, "An error occurred during registration. Please try again.")
+            return redirect("ecomapp:customerregistration")
+
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        if "next" in self.request.GET:
+            next_url = self.request.GET.get("next")
+            return next_url
+        else:
+            return self.success_url
+        
+
+    
+class CustomerLogoutView(View):
+    def get(self,request):
+        logout(request)
+        return redirect("ecomapp:home")
+    
+
+class CustomerLoginView(FormView):
+    template_name = "customerlogin.html"
+    form_class = CustomerLoginForm
+    success_url = reverse_lazy("ecomapp:home")
+    
+    #form_valid method is a type of post method and ois available in createview,formview and updateview
+    def form_valid(self,form):
+        uname = form.cleaned_data.get("username")
+        pword = form.cleaned_data["password"]
+        usr = authenticate(username=uname,password=pword)
+        if usr is not None and usr.customer:
+            login(self.request, usr)
+        else:
+            return render(self.request,self.template_name, {"form": self.form_class,"error": "Invalid credentials"})
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        if "next" in self.request.GET:
+            next_url = self.request.GET.get("next")
+            return next_url
+        else:
+            return self.success_url
+
+
+class AboutView(EcomMixin,TemplateView):
+    template_name= "about.html"
+
+class ContactView(EcomMixin,TemplateView):
+    template_name= "contactus.html"
+
+class CustomerProfileView(TemplateView):
+    template_name="customerprofile.html"
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        if request.user.is_authenticated and request.user.customer:
+            pass
+        else:
+            return redirect("/login/?next=/profile/")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        customer=self.request.user.customer
+        context['customer']=customer
+        orders=Order.objects.filter(cart__customer=customer).order_by("-id")
+        context['orders']=orders
+        return context
